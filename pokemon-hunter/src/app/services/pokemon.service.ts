3a8183eca4e4c754b533a45c4e0a85224ed13929
benp3837 <petruzziellbj@alumni.vcu.edu>
2025-05-21 09:29:21 -0400

    mostly done with catch HTML

--- pokemon-hunter/src/app/services/pokemon.service.ts
+++ pokemon-hunter/src/app/services/pokemon.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { Pokemon } from '../interfaces/pokemon.interface';
 
 @Injectable({
@@ -19,10 +19,18 @@ export class PokemonService {
  //TODO: this is harcoded to pikachu for now
   getRandomPokemon():Observable<Pokemon> {
     
-    /* send an HTTP GET request to the pokeAPI, and convert the data into a Pokemon object */
-    return this.http.get<Pokemon>("https://pokeapi.co/api/v2/pokemon/pikachu")
+    /* send an HTTP GET request to the pokeAPI, and convert the data into a Pokemon object 
+    We'll use the .pipe() method to transform the incoming data into our Pokemon object 
+    we'll use the map() method to take the data and fit it into our Pokemon fields*/
+    return this.http.get<any>("https://pokeapi.co/api/v2/pokemon/pikachu").pipe(
+      map(data => ({
+        id:data.id,
+        name:data.name,
+        sprite:data.sprites.front_default
+      }))
+      //THIS^ is our Pokemon object we're returning based off the PokeAPI data
+    )
 
-    //TODO: need to use pipe() to convert the incoming pokemon data into our version of a Pokemon (the Pokemon interface)
 
   }
  
