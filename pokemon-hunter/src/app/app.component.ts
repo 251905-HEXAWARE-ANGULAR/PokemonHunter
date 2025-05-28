703e37f585eeaf0c43e7d698acf525d4533a2b20
benp3837 <petruzziellbj@alumni.vcu.edu>
2025-05-28 13:57:40 -0400

    navbar hidden on login page

--- pokemon-hunter/src/app/app.component.ts
+++ pokemon-hunter/src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'pokemon-hunter';
+
+  //Inject Router for our conditional rendering with *ngIf 
+  //This will let us hide the navbar on the login component (when "path" is "/")
+  constructor(public router:Router){}
+
 }
