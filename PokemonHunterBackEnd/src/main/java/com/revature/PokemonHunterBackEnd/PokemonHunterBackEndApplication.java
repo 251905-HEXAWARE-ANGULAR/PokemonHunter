f49d74dd62dbc40ee4a7b73d0b550eafdf1d55b7
benp3837 <petruzziellbj@alumni.vcu.edu>
2025-05-29 13:11:38 -0400

    init spring boot backend

---
+++ PokemonHunterBackEnd/src/main/java/com/revature/PokemonHunterBackEnd/PokemonHunterBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.revature.PokemonHunterBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonHunterBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonHunterBackEndApplication.class, args);
+	}
+
+}
---
+++ PokemonHunterBackEnd/src/test/java/com/revature/PokemonHunterBackEnd/PokemonHunterBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.PokemonHunterBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonHunterBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
